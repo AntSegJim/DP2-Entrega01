f847bfe57e3dc0b8443602ab31a8e2c77b595242
crilorbre <43986572+crilorbre@users.noreply.github.com>
2019-03-04 16:24:18 +0100

    Proyecto Plantilla
    Aádido al proyecto plantilla https y la funcion para notificaciones

---
+++ Sample Project 1.19/src/main/java/controllers/NotificationAdministratorController.java
@@ -0,0 +1,74 @@
+
+package controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.NotificationService;
+import domain.Notification;
+
+@Controller
+@RequestMapping("/notification/administrator")
+public class NotificationAdministratorController {
+
+	@Autowired
+	private NotificationService	notificationService;
+
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		final ModelAndView result;
+		final Notification notification;
+
+		notification = this.notificationService.create();
+		Assert.notNull(notification);
+
+		result = new ModelAndView("notification/edit");
+		result.addObject("notification", notification);
+		return result;
+
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int notificationId) {
+		ModelAndView result;
+		final Notification notification;
+
+		notification = this.notificationService.findOne(notificationId);
+
+		result = new ModelAndView("notification/edit");
+		result.addObject("notification", notification);
+
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView edit(@Valid final Notification notification, final BindingResult binding) {
+		ModelAndView result;
+
+		try {
+			if (!binding.hasErrors()) {
+				this.notificationService.save(notification);
+				result = new ModelAndView("redirect:https://localhost:8443/Sample-Project/notification/actor/list.do");
+			} else {
+				result = new ModelAndView("notification/edit");
+				result.addObject("notification", notification);
+			}
+		} catch (final Exception e) {
+			result = new ModelAndView("notification/edit");
+			result.addObject("exception", e);
+			result.addObject("notification", notification);
+		}
+
+		return result;
+	}
+
+}
