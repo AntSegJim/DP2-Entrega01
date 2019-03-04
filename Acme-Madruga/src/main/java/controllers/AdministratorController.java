12b81bfdb07273f41ad2037442125226f1e7a46c
jeselirod <jesuselia71@gmail.com>
2019-03-04 09:34:48 +0100

    Nuevo Administrador

--- Acme-Madruga/src/main/java/controllers/AdministratorController.java
+++ Acme-Madruga/src/main/java/controllers/AdministratorController.java
@@ -14,27 +14,36 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.AdministratorService;
 import services.BrotherhoodService;
 import services.PositionService;
 import services.ProcessionService;
+import domain.Administrator;
 
 @Controller
 @RequestMapping("/administrator")
 public class AdministratorController extends AbstractController {
 
 	@Autowired
-	private PositionService		positionService;
+	private PositionService			positionService;
 
 	@Autowired
-	private ProcessionService	processionService;
+	private ProcessionService		processionService;
 
 	@Autowired
-	private BrotherhoodService	brotherhoodService;
+	private BrotherhoodService		brotherhoodService;
+
+	@Autowired
+	private AdministratorService	administratorService;
 
 
 	// Constructors -----------------------------------------------------------
@@ -79,6 +88,39 @@ public class AdministratorController extends AbstractController {
 
 		return result;
 
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView action1() {
+		ModelAndView result;
+		final Administrator administrator;
+
+		administrator = this.administratorService.create();
+
+		result = new ModelAndView("administrator/create");
+		result.addObject("administrator", administrator);
+
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView edit(@Valid final Administrator administrator, final BindingResult binding) {
+		ModelAndView result;
+		try {
+			if (!binding.hasErrors()) {
+				this.administratorService.save(administrator);
+				result = new ModelAndView("redirect:https://localhost:8443/Acme-Madruga");
+			} else {
+				result = new ModelAndView("administrator/create");
+				result.addObject("administrator", administrator);
+			}
+		} catch (final Exception e) {
+			result = new ModelAndView("administrator/create");
+			result.addObject("exception", e);
+			result.addObject("administrator", administrator);
+		}
+
+		return result;
 	}
 
 }
